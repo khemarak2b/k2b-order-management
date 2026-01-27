7c4bffc41a253051b74cc6bd4ac3b21420772f1c
Himesh Vithanagama <himesh.vithanagama@ebuilder.com>
2026-01-27 09:37:24 +0530

    allowedHeaders updated

--- amplify/backend/function/adminOrdersHandler/src/index.js
+++ amplify/backend/function/adminOrdersHandler/src/index.js
@@ -32,7 +32,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "X-Amz-Date", "Authorization", "X-Api-Key", "X-Amz-Security-Token"],
+  allowedHeaders: ["Content-Type", "X-Amz-Date", "Authorization", "X-Api-Key", "X-Amz-Security-Token", "custom-token"],
 };
 
 app.use(cors(corsOptions));
